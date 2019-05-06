b18c96eb5270e84a6fb22c8fc5b9c9462915063b
emialex40 <26emic73@gmail.com>
2019-05-06 18:31:30 +0300

    first commit

---
+++ src/js/common.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function ($) {
+
+  $('h1').css('color', 'green');
+
+  $('[data-fancybox]').fancybox({
+    baseClass: 'fancybox-custom-layout',
+    margin: 0
+  });
+
+});
+  
+  
+  
