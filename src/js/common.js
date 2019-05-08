9f338cd84c7e960fd8fb294deb5c52f541d243a0
emialex40 <26emic73@gmail.com>
2019-05-08 14:23:59 +0300

    actions section desctop 8.05.2019

--- src/js/common.js
+++ src/js/common.js
@@ -5,7 +5,36 @@ jQuery(document).ready(function ($) {
     margin: 0
   });
 
+  // section actions enents
+  var act = $('.js-fade');
+
+  act.click(function () {
+    if(!$(this).hasClass('active')) {
+      $(this).addClass('active');
+      // class cross manipulate
+      $('.cross__verl').removeClass('rotate');
+      $(this).children('.cross').children('.cross__verl').addClass('rotate');
+      // class action__title hide
+      $(this).children('.actions__title').fadeOut(500);
+      var numb = $(this).data('numb');
+
+      // class tip action
+      $(this).siblings('.tip').fadeOut(500);
+      $('[data-accord="' + numb + '"]').fadeIn(700);
+
+      $(this).siblings('.js-fade').removeClass('active');
+      $(this).siblings('.js-fade').children('.cross').children('.cross__verl').removeAttr('style');
+      $(this).siblings('.js-fade').children('.actions__title').fadeIn(700);
+
+    } else {
+      act.removeClass('active');
+      act.children('.cross').children('.cross__verl').removeClass('rotate');
+      act.children('.cross').children('.cross__verl').removeAttr('style');
+      act.siblings('.tip').fadeOut(500);
+      act.children('.actions__title').fadeIn(700);
+    }
+
+  });
+
+
 });
-  
-  
-  
